296f825abdadd3e6d14ac286a4c9d953a75dd57d
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-04-01 21:02:56 +0100

    Render the app titles in the two colums

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -9,7 +10,7 @@ with col1:
 
 with col2:
     st.title("Alina Rozhko")
-    content = """🔴 Alina Rozhko – QA Engineer | Python Automation | Freelance 🔴
+    content = """🔴 Alina  – QA Engineer | Python Automation | Freelance 🔴
 
 I am an experienced QA Engineer with a passion for quality and efficiency in software testing. Currently expanding my skills by learning Python for automation and freelance projects. My goal is to create reliable, scalable, and efficient testing solutions that streamline development and improve product quality.
 
@@ -21,3 +22,15 @@ With hands-on experience in manual and automated testing, I focus on delivering 
 content2 = """Bellow you can find some of the apps I have built on Python. 
 If you any additional queries -  Feel free to contact me!"""
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
