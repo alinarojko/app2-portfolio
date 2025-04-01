c88ed2da8101c2bd9659b43c4354133cfef6acdc
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-04-01 11:03:07 +0100

    Add description on the new column

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ with col1:
 
 with col2:
     st.title("Alina Rozhko")
-    content = """"Alina Rozhko – QA Engineer with experience in testing and automation. 
-    Learning Python and applying it to develop reliable and high-quality solutions."""
+    content = """🔴 Alina Rozhko – QA Engineer | Python Automation | Freelance 🔴
+
+I am an experienced QA Engineer with a passion for quality and efficiency in software testing. Currently expanding my skills by learning Python for automation and freelance projects. My goal is to create reliable, scalable, and efficient testing solutions that streamline development and improve product quality.
+
+With hands-on experience in manual and automated testing, I focus on delivering high-quality software through robust testing strategies. Always eager to learn, adapt, and implement the best practices in QA and automation. 🚀
+
+📌 LinkedIn Profile"""
     st.write(content)
+
+content2 = """Bellow you can find some of the apps I have built on Python. 
+If you any additional queries -  Feel free to contact me!"""
+st.write(content2)
